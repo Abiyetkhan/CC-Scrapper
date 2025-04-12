70c69c8696c890fda9df4c621808c4999b3dbfe1
abiyet <102137424+Abiyetkhan@users.noreply.github.com>
2025-04-12 22:22:06 +0300

    config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 # Pyrogram setup
-API_ID = "123456"  # Replace this API ID with your actual API ID
-API_HASH = "XXXXXXXXXXX"  # Replace this API HASH with your actual API HASH
-SESSION_STRING = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"  # Replace this SESSION STRING with your actual SESSION_STRING
-BOT_TOKEN = "12345678:XXXXXXXXXXXXXX"  # Replace this BOT_TOKEN
+API_ID = "20504079"  # Replace this API ID with your actual API ID
+API_HASH = "e9a75464f100c1b61efd376d294909eb"  # Replace this API HASH with your actual API HASH
+SESSION_STRING = "1BJWap1sBu36lg0C3gy4ctJAU3tMCqmQQyHlx_zErcYZRzKxXz5IT8OJXvETGT0D0N1OUiczRAfba37FjmgnFv35NacI5fnw9uMfzIObzK7nHWDDIkd7QA3fChF8UvLP0rEa2jbfJJHY09cq2Dzyf3dGNiJziL9jL8he0mJEA9vGNZhoDUUgtwa9PM4-u8tVlmuh59wkdGspaiJE3DE1KvGyEZXjQdIyP-U0yBfB7jRAisQw2jx6uE6Hs2vKZJyCHwmEiGvPzMb01KMqwllRju1-tpRSnfFGF7k4yvZuwJ-qpQziMHtgMqIJq48rE2gZZNl5ucaqbpixvFMXci3yeajnsk4i-h60="  # Replace this SESSION STRING with your actual SESSION_STRING
+BOT_TOKEN = "7675936643:AAECmBmBE6aNeHfMrgLvwaAK0jrLQm7kcMY"  # Replace this BOT_TOKEN
 
 # Admin IDs
-ADMIN_IDS = [12345678, 12345678]
+ADMIN_IDS = [8011959413]
 
 # Limits
 DEFAULT_LIMIT = 10000 # Card Scrapping Limit For Everyone
